0d1b21a2208767bdcf8e47623d3ea6c6aa66e07a
CXaymae <morabiitchaima@gmail.com>
2024-07-17 23:00:20 +0200

    Terminal commands automation with subprocess library

--- Python/Day 1/PYTHON_FOR_AUTOMATION/pratice.py
+++ Python/Day 1/PYTHON_FOR_AUTOMATION/pratice.py
@@ -2,3 +2,5 @@ import subprocess
 
 for i in range(5):
     subprocess.check_call(["python3", "example.py"])
+
+    # check_call it runs an executable in the terminal and waits until the process it has finished before continuing the scripting 
